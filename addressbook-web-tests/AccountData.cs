55e54bb585a312ee86eac6d972abb8921b8cdb20
Amir771 <amir7@bictimirov@gmail.com>
2023-02-09 00:47:19 +0300

    Провел реструктуризацию и выделил классы

---
+++ addressbook-web-tests/AccountData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    internal class AccountData
+    {
+        private string username;
+        private string passwword;
+
+        public AccountData(string username, string passwword)
+        {
+            this.username = username;
+            this.passwword = passwword;
+        }
+        
+        public string Username
+        {
+            get
+            { 
+                return username; 
+            }
+            set
+            {
+                username = value;
+            }
+
+        }
+
+        public string Password
+        {
+            get
+            { 
+                return passwword;
+            }
+            set
+            { 
+                passwword = value; 
+            }
+        }
+    }
+}
